e6a6c1f16967c36cb7f26b87bc857a76d1b9ab2f
YoonSung Kim <opqudk@gmail.com>
2022-03-06 15:54:33 +0900

    6.3 To and from Strings

---
+++ src/06_conversion/03_to_and_from_strings.rs
@@ -0,0 +1,24 @@
+use std::fmt;
+
+struct Circle {
+    radius: i32
+}
+
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.radius)
+    }
+}
+
+fn to_and_from_strings() {
+    // Converting to String
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    // Parsing a String
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
+}
