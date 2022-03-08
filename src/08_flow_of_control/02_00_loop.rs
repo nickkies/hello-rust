dc469b3daceda409b316ce2a60fabd31c5435c4d
YoonSung Kim <opqudk@gmail.com>
2022-03-09 08:09:47 +0900

    8.2. loop

---
+++ src/08_flow_of_control/02_00_loop.rs
@@ -0,0 +1,24 @@
+fn _loop() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // 없어도 됨
+            // continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("Ok, that's enough");
+
+            break;
+        }
+    }
+}
