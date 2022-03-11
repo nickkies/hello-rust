eca3c081ee8ff9420446e7db21a11e34b4f976f8
YoonSung Kim <opqudk@gmail.com>
2022-03-11 09:55:59 +0900

    8.5.1.2 arrays/slices

---
+++ src/08_flow_of_control/05_01_02_arrays_slices.rs
@@ -0,0 +1,26 @@
+fn arrays_slices() {
+    let array = [1, -2, 6];
+
+    match array {
+        [0, second, third] =>
+            println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+        [2, _, third] => println!(
+            "array[0] = 1, array[2] = {}, and array[1] was ignored",
+            third
+        ),
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // not compile
+        // [-1, second] => ...
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+        [first, middle @.., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
