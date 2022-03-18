125cb3a49bce71c7711006d195b4661a70ed2b86
YoonSung Kim <opqudk@gmail.com>
2022-03-18 14:43:00 +0900

    9.2.1. Capturing

---
+++ src/09_functions/02_01_capturing.rs
@@ -0,0 +1,39 @@
+fn capturing() {
+    use std::mem;
+
+    let color = String::from("green");
+
+    let print = || println!("`color`: {}", color);
+
+    print();
+
+    let _reborrow = &color;
+    print();
+
+    let _color_moved = color;
+
+    let mut count = 0;
+
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    inc();
+
+    // let _reborrow = &count;
+
+    inc();
+
+    let _count_reborrowed = &mut count;
+
+    let movable = Box::new(3);
+
+    let consume = || {
+        println!("`movable: {:?}", movable);
+        mem::drop(movable);
+    };
+
+    consume();
+    // consume();
+}
