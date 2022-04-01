4c783512d2ebf1e4aeaf3d2cc9942fa9a99f6e17
YoonSung Kim <opqudk@gmail.com>
2022-04-01 10:38:55 +0900

    14.3. Traits

---
+++ src/14_generics/03_traits.rs
@@ -0,0 +1,21 @@
+struct Empty;
+struct Null;
+
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+}
+
+impl<T, U> DoubleDrop<T> for U {
+    fn double_drop(self, _: T) {}
+}
+
+fn traits() {
+    let empty = Empty;
+    let null = Null;
+
+    empty.double_drop(null);
+
+    // empty;
+    // null;
+
+}
