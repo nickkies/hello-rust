5bc3978e5b702566a5896fe500c52a0c85c9cdd7
YoonSung Kim <opqudk@gmail.com>
2022-05-02 17:29:01 +0900

    15.1. RAII

---
+++ src/15_scoping_rules/01_raii.rs
@@ -0,0 +1,28 @@
+// Resource Acquisition Is Initialization
+/*
+// raii.rs
+fn create_box() {
+    let _box1 = Box::new(3i32);
+}*/
+
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+fn raii() {
+    /*let _box2 = Box::new(5i32);
+
+    {
+        let _box3 = Box::new(4i32);
+    }
+
+    for _ in 0u32..1_000 {
+        create_box();
+    }*/
+
+    let _x = ToDrop;
+    println!("Made a ToDrop!");
+}
