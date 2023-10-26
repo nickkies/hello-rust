7d63b24562d1c971fe218a0817a2459dcae8c7ea
YoonSung Kim <opqudk@gmail.com>
2023-10-26 10:16:48 +0900

    variables

--- 1.Kibon/variables/src/main.rs
+++ 1.Kibon/variables/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    // definition
+    let a = 5;
+
+    println!("a is: {a}");
+
+    // mutability
+    let mut b = 5;
+    b = 10;
+
+    println!("b is now: {b}");
+
+    // shadowing
+    let c = 10;
+    let c = 20;
+
+    println!("c is now: {c}");
+
+    // scope
+    let d = 30;
+
+    {
+        let d = 40;
+        println!("d is now: {d}");
+    }
+
+    println!("d is still: {d}");
 }
