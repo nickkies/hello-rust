39fe836be6b0aaf6f351b948672942909671f81f
YoonSung Kim <opqudk@gmail.com>
2023-10-27 10:04:35 +0900

    functions

---
+++ 1.Kibon/functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let z = my_function(22);
+    println!("my_function returned: {z}");
+}
+
+fn my_function(x: i32) -> i32 {
+    println!("my_function called with: {x}");
+    x + 1
+}
