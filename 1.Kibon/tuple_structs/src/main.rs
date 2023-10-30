aec50c586f44d1447a9a6d8420025c3c0b0022c3
YoonSung Kim <opqudk@gmail.com>
2023-10-31 07:11:47 +0900

    tuple

---
+++ 1.Kibon/tuple_structs/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let rgb_color = (255, 255, 255);
+  let rgba_color = (255, 255, 255, 0.5);
+
+  sturct RGB(i32, i32, i32);
+  struct RGBA(i32, i32, i32, f32);
+
+  let _color1 = RGB(255, 255, 255);
+  let _color2 = RGBA(255, 255, 255, 0.5);
+
+  struct MyStruct;
+}
