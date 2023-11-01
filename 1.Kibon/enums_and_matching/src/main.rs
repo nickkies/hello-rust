edf77795006f9fae6508389d7d36727527bc0bd9
YoonSung Kim <opqudk@gmail.com>
2023-11-01 15:38:05 +0900

    matching

--- 1.Kibon/enums_and_matching/src/main.rs
+++ 1.Kibon/enums_and_matching/src/main.rs
@@ -21,7 +21,36 @@ enum Command {
 
 impl Command {
     fn serialize(&self) -> String {
-        String::from("TODO")
+        match self {
+            Command::Undo => String::from("{ \"cmd\": \"undo\" }"),
+            Command::Redo => String::from("{ \"cmd\": \"redo\" }"),
+            Command::AddText(s) => {
+                format!(
+                    "{{ \
+                    \"cmd\": \"add_text\", \
+                    \"text\": \"{s}\" \
+                }}"
+                )
+            }
+            Command::MoveCursor(line, column) => {
+                format!(
+                    "{{ \
+                    \"cmd\": \"move_cursor\", \
+                    \"line\": {line}, \
+                    \"column\": {column} \
+                }}"
+                )
+            }
+            Command::Replace { from, to } => {
+                format!(
+                    "{{ \
+                    \"cmd\": \"replace\", \
+                    \"from\": \"{from}\", \
+                    \"to\": \"{to}\", \
+                }}"
+                )
+            }
+        }
     }
 }
 
@@ -33,6 +62,14 @@ fn main() {
         in_stock: true,
     };
 
+    let age = 35;
+
+    match age {
+        1 => println!("Happy 1st Birthday!"),
+        13..=19 => println!("You are a teenager!"),
+        x => println!("You are {x} years old!"),
+    }
+
     let cmd1 = Command::Undo;
     let cmd2 = Command::AddText(String::from("hello"));
     let cmd3 = Command::MoveCursor(22, 0);
@@ -40,4 +77,9 @@ fn main() {
         from: String::from("a"),
         to: String::from("b"),
     };
+
+    println!("{}", cmd1.serialize());
+    println!("{}", cmd2.serialize());
+    println!("{}", cmd3.serialize());
+    println!("{}", cmd4.serialize());
 }
