d839a58f1e71a273b610350e84555942522d0b4f
YoonSung Kim <opqudk@gmail.com>
2023-11-06 10:52:28 +0900

    external dependencies

--- 1.Kibon/modules/src/lib.rs
+++ 1.Kibon/modules/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code, unused_variables)]
+use rand::prelude::*;
 
 mod auth_utils;
 mod database;
@@ -7,6 +8,10 @@ pub use auth_utils::models::Credentials;
 use database::Status;
 
 pub fn authenticate(creds: Credentials) {
+    let timeout = thread_rng().gen_range(100..500);
+
+    println!("The timeout is {timeout}");
+
     if let Status::Connected = database::connect_to_database() {
         auth_utils::login(creds);
     }
