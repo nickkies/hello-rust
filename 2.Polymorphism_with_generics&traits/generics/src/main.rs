8e1911cbbe71ee992219b5b936c160508142e9fa
YoonSung Kim <opqudk@gmail.com>
2023-11-11 14:56:09 +0900

    :fire: generics :fire:

---
+++ 2.Polymorphism_with_generics&traits/generics/src/main.rs
@@ -0,0 +1,37 @@
+struct BrowserCommand<T> {
+    name: String,
+    payload: T,
+}
+
+impl<T> BrowserCommand<T> {
+    fn new(name: String, payload: T) -> Self {
+        Self { name, payload }
+    }
+
+    fn get_payload(&self) -> &T {
+        &self.payload
+    }
+}
+
+impl BrowserCommand<String> {
+    fn print_payload(&self) {
+        println!("{}", self.payload);
+    }
+}
+
+fn main() {
+    let cmd1 = BrowserCommand::new("navigate".to_owned(), "https://www.rust.com".to_owned());
+    let cmd2 = BrowserCommand::new("zoom".to_owned(), 200);
+
+    cmd1.print_payload();
+
+    let p1 = cmd1.get_payload();
+    let p2 = cmd2.get_payload();
+
+    serialize_payload(p1);
+    serialize_payload(p2);
+}
+
+fn serialize_payload<T>(payload: &T) -> String {
+    "placeholder".to_owned()
+}
