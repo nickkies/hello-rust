d58583327e869609301b695d0dded3e5ee75df08
YoonSung Kim <opqudk@gmail.com>
2023-11-21 15:28:06 +0900

    :fire: refcell smart pointer :fire:

--- 3.Memory_management/rc_smart_pointer/src/main.rs
+++ 3.Memory_management/rc_smart_pointer/src/main.rs
@@ -1,17 +1,27 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
-struct Database {}
+struct Database {
+    max_connections: u32,
+}
 
 struct AuthService {
-    db: Rc<Database>,
+    db: Rc<RefCell<Database>>,
 }
 
 struct ContentService {
-    db: Rc<Database>,
+    db: Rc<RefCell<Database>>,
 }
 
 fn main() {
-    let db = Rc::new(Database {});
+    let db = Rc::new(RefCell::new(Database {
+        max_connections: 100,
+    }));
     let auth_service = AuthService { db: Rc::clone(&db) };
     let content_service = ContentService { db: Rc::clone(&db) };
+
+    let r1 = db.borrow_mut();
+    let mut r2 = db.borrow_mut(); // panicked already borrowed: BorrowMutError
+
+    r2.max_connections = 200;
 }
