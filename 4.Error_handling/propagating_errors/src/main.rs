5f91a513ff28cb3a709348833bdb9d02ef0f0196
YoonSung Kim <opqudk@gmail.com>
2023-11-24 10:28:46 +0900

    :fire: propagating errors :fire:

---
+++ 4.Error_handling/propagating_errors/src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+fn main() {
+    let contents = read_file("example.txt");
+}
+
+fn read_file(filename: &str) -> Result<String, io::Error> {
+    let mut contents = String::new();
+    File::open(filename)?.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+struct User {
+    firstname: String,
+    lastname: String,
+}
+
+fn get_initails(user: User) -> Option<String> {
+    let first_initial = user.firstname.chars().next()?;
+    let last_initial = user.lastname.chars().next()?;
+    Some(format!("{first_initial}{last_initial}"))
+}
