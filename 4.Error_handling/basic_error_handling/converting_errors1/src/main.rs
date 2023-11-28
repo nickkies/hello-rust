cfc3ec48da48aeca625f50383361c2a2a745db9e
YoonSung Kim <opqudk@gmail.com>
2023-11-28 09:18:33 +0900

    :monocle_face: converting errors1 :monocle_face:

---
+++ 4.Error_handling/basic_error_handling/converting_errors1/src/main.rs
@@ -0,0 +1,28 @@
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+enum AddError {
+    ParseError(ParseIntError),
+    Overflow,
+}
+
+impl From<ParseIntError> for AddError {
+    fn from(value: ParseIntError) -> Self {
+        Self::ParseError(value)
+    }
+}
+
+fn add(num1: &str, num2: &str) -> Result<u8, AddError> {
+    let num1 = num1.parse::<u8>()?;
+    let num2 = num2.parse::<u8>()?;
+    num1.checked_add(num2).ok_or(AddError::Overflow)
+}
+
+fn main() {
+    // let (input1, input2) = ("23", "50");
+    let (input1, input2) = ("23", "dd");
+    match add(input1, input2) {
+        Ok(res) => println!("{input1} + {input2} = {res}"),
+        Err(e) => println!("Error: {e:?}"),
+    }
+}
