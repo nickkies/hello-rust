5b904083f5997cb6e5a756070c7682cc2debf513
YoonSung Kim <opqudk@gmail.com>
2023-12-01 14:49:01 +0900

    :fire: error stack :fire:

--- 4.Error_handling/basic_error_handling/error_stack/src/main.rs
+++ 4.Error_handling/basic_error_handling/error_stack/src/main.rs
@@ -1,4 +1,33 @@
-use std::{collections::HashMap, io};
+#![allow(dead_code)]
+
+use error_stack::{Report, Result, ResultExt};
+use std::{collections::HashMap, error::Error, fmt, io};
+
+#[derive(Debug)]
+struct ParsePaymentInfoError;
+
+impl fmt::Display for ParsePaymentInfoError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.write_str("Parsing payment error: invalid payment info")
+    }
+}
+
+impl Error for ParsePaymentInfoError {}
+
+fn parse_card_numbers(card: &str) -> Result<Vec<u32>, ParsePaymentInfoError> {
+    let numbers = card
+        .split(" ")
+        .map(|s| {
+            s.parse::<u32>()
+                .attach_printable_lazy(|| format!("{s:?} could not be parsed as u32"))
+                .change_context(ParsePaymentInfoError)
+        })
+        .collect::<Result<Vec<u32>, _>>()
+        .change_context(ParsePaymentInfoError)
+        .attach_printable(format!("Failed to parse input as numbers. Input: {card}"))?;
+
+    Ok(numbers)
+}
 
 #[derive(Debug)]
 struct Expiration {
@@ -13,45 +42,68 @@ struct Card {
     cvv: u32,
 }
 
-fn parse_card_numebers(card: &str) -> Vec<u32> {
-    let numbers = card
-        .split(" ")
-        .into_iter()
-        .map(|s| s.parse())
-        .collect::<Result<Vec<u32>, _>>()
-        .unwrap();
+fn parse_card(card: &str) -> Result<Card, ParsePaymentInfoError> {
+    let mut numbers = parse_card_numbers(card)?;
 
-    numbers
-}
+    let len = numbers.len();
+    let expected_len = 4;
 
-fn parse_card(card: &str) -> Card {
-    let mut numbers = parse_card_numebers(card);
+    if len != expected_len {
+        return Err(Report::new(ParsePaymentInfoError)
+            .attach_printable(format!(
+                "Incorrect number of elements parsed. Expected {expected_len} but got {len}. Elements: {numbers:?}"
+            )));
+    }
 
     let cvv = numbers.pop().unwrap();
     let year = numbers.pop().unwrap();
     let month = numbers.pop().unwrap();
     let number = numbers.pop().unwrap();
 
-    Card {
+    Ok(Card {
         number,
         exp: Expiration { year, month },
         cvv,
+    })
+}
+
+#[derive(Debug)]
+enum CreditCardError {
+    InvalidInput(String),
+    Other,
+}
+
+impl fmt::Display for CreditCardError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.write_str("Credit card error: Could not retrieve credit card.")
     }
 }
 
-fn get_credit_card_info(credit_cards: &HashMap<&str, &str>, name: &str) -> Card {
-    let card_string = credit_cards.get(name).unwrap();
+impl Error for CreditCardError {}
 
-    let card = parse_card(card_string);
+fn get_credit_card_info(
+    credit_cards: &HashMap<&str, &str>,
+    name: &str,
+) -> Result<Card, CreditCardError> {
+    let card_string = credit_cards.get(name).ok_or_else(|| {
+        let msg = format!("No credit card was found for {name}.");
+        Report::new(CreditCardError::InvalidInput(msg.clone())).attach_printable(msg.clone())
+    })?;
 
-    card
+    let card = parse_card(card_string)
+        .change_context(CreditCardError::Other)
+        .attach_printable(format!("{name}'s card could not be parsed."))?;
+
+    Ok(card)
 }
 
 fn main() {
+    env_logger::init();
+
     let credit_cards = HashMap::from([
-        ("Amy", "1234567 12 16 123"),
-        ("Tim", "1234567 06 06"),
-        ("Bob", "1234567 Dec 08 123"),
+        ("Amy", "1234567 04 25 123"),
+        ("Tim", "1234567 06 27"),
+        ("Bob", "1234567 Dec 27 123"),
     ]);
 
     println!("Enter Name: ");
@@ -64,5 +116,17 @@ fn main() {
 
     let result = get_credit_card_info(&credit_cards, name.trim());
 
-    println!("\nCredit Card Info: {result:?}");
+    match result {
+        Ok(card) => {
+            println!("\nCredit Card Info: {card:?}");
+        }
+        Err(err) => {
+            match err.current_context() {
+                CreditCardError::InvalidInput(msg) => println!("\n{msg}"),
+                CreditCardError::Other => println!("\nSomething went wrong! Try again!"),
+            }
+
+            log::error!("\n{err:?}");
+        }
+    }
 }
