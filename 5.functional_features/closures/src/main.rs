0152f8ef304e4b85cf5eccac99731382ea771f2d
YoonSung Kim <opqudk@gmail.com>
2023-12-04 10:33:04 +0900

    :fire: closures

--- 5.functional_features/closures/src/main.rs
+++ 5.functional_features/closures/src/main.rs
@@ -32,7 +32,6 @@ fn main() {
             && password.contains(['!', '@', '#', '$', '%', '^', '&', '*'])
             && password != weak_password
     };
-
     println!("{weak_password}");
 
     let creds = Credentials {
@@ -41,8 +40,36 @@ fn main() {
         validator: validator2,
     };
     println!("{}", creds.is_valid());
+
+    let password_validator = get_password_validator(8, true);
+    let default_creds = get_default_creds(password_validator);
+
+    println!("{}", default_creds.is_valid());
 }
 
 fn _validate_credentials(username: &str, password: &str) -> bool {
     !username.is_empty() && !password.is_empty()
+}
+
+fn get_default_creds<T>(f: T) -> Credentials<T>
+where
+    T: Fn(&str, &str) -> bool,
+{
+    Credentials {
+        username: "guest".to_string(),
+        password: "password123!".to_string(),
+        validator: f,
+    }
+}
+
+// fn get_password_validator(min_len: usize) -> impl Fn(&str, &str) -> bool {
+fn get_password_validator(min_len: usize, special_char: bool) -> Box<dyn Fn(&str, &str) -> bool> {
+    if special_char {
+        Box::new(move |_: &str, password: &str| {
+            !password.len() >= min_len
+                && password.contains(['!', '@', '#', '$', '%', '^', '&', '*'])
+        })
+    } else {
+        Box::new(move |_: &str, password: &str| !password.len() >= min_len)
+    }
 }
