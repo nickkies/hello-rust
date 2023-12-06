8409f2bd30917ecc03493b6846556e9a17e45136
YoonSung Kim <opqudk@gmail.com>
2023-12-06 11:15:33 +0900

    ⚗️into iterator

--- 5.functional_features/iterator_pattern/src/bin/into_iterator.rs
+++ 5.functional_features/iterator_pattern/src/bin/into_iterator.rs
@@ -4,7 +4,7 @@ struct Employee {
     id: String,
 }
 
-struct EmployeeIter {
+/* struct EmployeeIter {
     state: Vec<String>,
 }
 
@@ -17,15 +17,28 @@ impl Iterator for EmployeeIter {
             Some(self.state.remove(0))
         }
     }
-}
+} */
 
-impl IntoIterator for Employee {
+/* impl IntoIterator for Employee {
     type Item = String;
     type IntoIter = EmployeeIter;
     fn into_iter(self) -> Self::IntoIter {
         EmployeeIter {
             state: vec![self.first_name, self.last_name, self.id],
         }
+    }
+} */
+
+impl IntoIterator for Employee {
+    type Item = String;
+    type IntoIter = std::vec::IntoIter<String>;
+    fn into_iter(self) -> Self::IntoIter {
+        vec![
+            format!("First name: {}", self.first_name),
+            format!("Last name: {}", self.last_name),
+            format!("id: {}", self.id),
+        ]
+        .into_iter()
     }
 }
 
@@ -35,9 +48,14 @@ fn main() {
         last_name: "Smith".to_string(),
         id: "1232".to_string(),
     };
-    let mut emp_iter = employee.into_iter();
+
+    /* let mut emp_iter = employee.into_iter();
     println!("First name: {}", emp_iter.next().unwrap());
     println!("Last name: {}", emp_iter.next().unwrap());
     println!("id: {}", emp_iter.next().unwrap());
-    assert_eq!(emp_iter.next(), None);
+    assert_eq!(emp_iter.next(), None); */
+
+    for detail in employee {
+        println!("{detail}");
+    }
 }
