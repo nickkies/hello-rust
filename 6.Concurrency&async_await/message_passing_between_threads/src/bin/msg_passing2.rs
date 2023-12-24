255b48c428fd614b19e61f9dad624398b067a2ac
YoonSung Kim <opqudk@gmail.com>
2023-12-24 19:49:36 +0900

    🎨 style: indent, type

--- 6.Concurrency&async_await/message_passing_between_threads/src/bin/msg_passing2.rs
+++ 6.Concurrency&async_await/message_passing_between_threads/src/bin/msg_passing2.rs
@@ -2,10 +2,10 @@ use std::{sync::mpsc, thread};
 
 fn main() {
     let sentences = [
-    "!tpircs llehs a eb ot detnaw eh esuaceB ?tsuR nrael barC eht sirreF did yhW".to_string(),
+        "!tpircs llehs a eb ot detnaw eh esuaceB ?tsuR nrael barC eht sirreF did yhW".to_string(),
         "!sgel sih fo thgie lla htiw tsuR ni edoc nac eh - reksat-itlum etamitlu eht si barC eht sirreF".to_string()
     ];
-    let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel::<String>();
 
     for sentence in sentences {
         let tx_clone = tx.clone();
@@ -22,5 +22,6 @@ fn main() {
             println!("{sentence}");
         }
     });
+
     printer.join().unwrap();
 }
