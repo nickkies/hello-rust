bb27f61cbd1a12e025fe9d7900edb1ec4c4aa8cf
YoonSung Kim <opqudk@gmail.com>
2024-01-02 14:28:22 +0900

    ✨ custom derive procedural macros

--- 7.Macro_system/procedural_macros/src/lib.rs
+++ 7.Macro_system/procedural_macros/src/lib.rs
@@ -3,6 +3,29 @@ extern crate proc_macro;
 use chrono::prelude::*;
 use proc_macro::TokenStream;
 use quote::quote;
+use syn;
+
+#[proc_macro_derive(Log)]
+pub fn log_derive(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+
+    let trait_impl = quote! {
+      impl Log for #name {
+        fn info(&self, msg: &str) {
+          println!("[INFO] {}: {}", stringify!(#name), msg);
+        }
+        fn warn(&self, msg: &str) {
+          println!("[WARN] {}: {}", stringify!(#name), msg);
+        }
+        fn error(&self, msg: &str) {
+          println!("[ERROR] {}: {}", stringify!(#name), msg);
+        }
+      }
+    };
+
+    trait_impl.into()
+}
 
 #[proc_macro]
 pub fn log_info(input: TokenStream) -> TokenStream {
--- 7.Macro_system/procedural_macros/src/main.rs
+++ 7.Macro_system/procedural_macros/src/main.rs
@@ -1,5 +1,41 @@
 use procedural_macros::*;
 
+trait Log {
+    fn info(&self, msg: &str);
+    fn warn(&self, msg: &str);
+    fn error(&self, msg: &str);
+}
+
+#[derive(Debug, Log)]
+struct Database {
+    url: String,
+    connections: u32,
+}
+
+impl Database {
+    fn new(url: String) -> Self {
+        Self {
+            url,
+            connections: 0,
+        }
+    }
+
+    fn connect(&mut self) {
+        self.info(format!("new connection to {}", self.url).as_str());
+        self.connections += 1;
+
+        if self.connections > 100 {
+            self.warn(format!("100 more connections open!").as_str());
+        }
+    }
+}
+
 fn main() {
     log_info!([TIME] starting program...);
+
+    let mut db = Database::new("localhost:8080".to_string());
+
+    for _ in 0..=100 {
+        db.connect();
+    }
 }
