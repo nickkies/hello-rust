3a0b4eab26fa931eae5e596b4e49b91d6cc99c31
YoonSung Kim <opqudk@gmail.com>
2024-02-17 07:36:11 +0900

    ✨ convert

--- 999.Stuff/write-binary-file/src/main.rs
+++ 999.Stuff/write-binary-file/src/main.rs
@@ -2,41 +2,40 @@ use serde_json::Value;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
+fn convert_file(file_path: &str) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
+    let mut json_file = File::open(file_path)?;
+    let mut json_data = String::new();
+    json_file.read_to_string(&mut json_data)?;
+
+    let data: Value = serde_json::from_str(&json_data)?;
+
+    let array = data["array"]
+        .as_array()
+        .ok_or("Failed to get array from JSON")?;
+
+    let res = array
+        .iter()
+        .flat_map(|arr| arr.as_array().unwrap().iter().cloned())
+        .map(|v| {
+            if let Value::Number(num) = v {
+                num.as_f64().unwrap_or_default() as f32
+            } else {
+                0.0
+            }
+        })
+        .collect::<Vec<f32>>();
+
+    Ok(res)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let arr = ["u", "v"];
 
-    arr.iter().for_each(|d| {
-        let mut json_file =
-            File::open(format!("data/{}.json", d)).expect("Failed to open JSON file");
-        let mut json_data = String::new();
-        json_file
-            .read_to_string(&mut json_data)
-            .expect("Failed to read JSON file");
-        let data: Value = serde_json::from_str(&json_data).expect("Failed to parse JSON");
+    for d in arr {
+        let x = convert_file(&format!("data/{}.json", d))?;
+        let mut bin_file = File::create(format!("data/{}.bin", d))?;
+        bin_file.write_all(bytemuck::cast_slice(&x))?;
+    }
 
-        let array = data["array"]
-            .as_array()
-            .expect("Failed to get array from JSON");
-
-        // TODO
-        let x: Vec<f64> = array
-            .iter()
-            .map(|v| {
-                if let Value::Number(num) = v {
-                    println!("{num}");
-                    num.as_f64().unwrap_or_default()
-                } else {
-                    0.0
-                }
-            })
-            .collect();
-
-        println!("{x:?}");
-
-        let mut bin_file =
-            File::create(format!("{}.bin", d)).expect("Failed to create binary file");
-        bin_file
-            .write_all(bytemuck::cast_slice(&x))
-            .expect("Failed to write binary file");
-    });
+    Ok(())
 }
